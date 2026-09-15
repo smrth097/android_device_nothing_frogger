7c11762bb097ac4928629e7898b3ca8acbb8bad4
Samarth <smrth097@gmail.com>
2026-09-15 13:22:52 +0530

    frogger: Import update from Frogger-B4.1-260808-1352
    Signed-off by: Samarth <smrth097@gmail.com>

--- lineage_frogger.mk
+++ lineage_frogger.mk
@@ -18,8 +18,8 @@ PRODUCT_NAME := lineage_frogger
 PRODUCT_GMS_CLIENTID_BASE := android-nothing
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 BQ2A.250721.001-BP2A.250605.031.A3 2606181048 release-keys" \
-    BuildFingerprint=Nothing/Asteroids/Asteroids:14/UKQ1.250612.001/2606181048:user/release-keys \
+    BuildDesc="qssi_64-user 16 BQ2A.250913.001-BP2A.250605.031.A3 2608081352 release-keys" \
+    BuildFingerprint=Nothing/Frogger/Frogger:14/UKQ1.250915.001/2608081352:user/release-keys \
     DeviceName=Frogger \
     DeviceProduct=Frogger \
     SystemDevice=Frogger \
