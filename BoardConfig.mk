c15533250d30d71335833d8c57928c03f9789f31
Samarth <smrth097@gmail.com>
2026-09-15 14:23:21 +0530

    frogger: Switch selinux to permisssive
    Signed-off by: Samarth <smrth097@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
     androidboot.load_modules_parallel=true \
     androidboot.memcg=1 \
+    androidboot.selinux=permissive \
     androidboot.usbcontroller=a600000.dwc3
 
 BOARD_KERNEL_BASE := 0x00000000
