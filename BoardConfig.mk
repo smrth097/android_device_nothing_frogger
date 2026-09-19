577335ef679e8ff49943ab87bb77d07f01e7ec7b
Samarth <smrth097@gmail.com>
2026-09-19 19:09:57 +0530

    frogger: Update configuration for Kbuild
    Signed-off by: Samarth <smrth097@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -135,7 +135,7 @@ TARGET_KERNEL_SOURCE := kernel/nothing/sm7635
 TARGET_KERNEL_CONFIG := \
     gki_defconfig \
     vendor/pineapple_perf.config \
-    vendor/asteroids_perf.config
+    vendor/frogger_perf.config
 TARGET_MERGE_DTBS_WILDCARD := *volcano*
 
 # Kernel Modules
